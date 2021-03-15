a0aac2461b16325299874ae58bb6f3537fbab007
Melissa Chung G <mchungg@ucenfotec.ac.cr>
2021-03-15 15:05:07 -0600

    avance con decorador en ataques especiales

---
+++ core/src/com/mygdx/game/bl/personajes/decorador/ObjetoPersonajeDecorado.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.bl.personajes.decorador;
+
+public class ObjetoPersonajeDecorado {
+}
---
+++ core/src/com/mygdx/game/bl/personajes/decoradoresConcretos/PersonajeVidaPor2Movimientos.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.bl.personajes.decoradoresConcretos;
+
+public class PersonajeVidaPor2Movimientos {
+}
